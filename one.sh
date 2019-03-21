aa5ce988f09b03538fa57525890ec81b712ac775
Sushil19 <37891431+Sushil19@users.noreply.github.com>
2019-03-21 16:55:15 +0530

    Add files via upload

---
+++ one.sh
@@ -0,0 +1,11 @@
+to search shell types :
+cat /etc/shells
+
+to know bash shell location  :
+which bash
+
+to change the permissons of file ie, mode :
+chmod +x filenae.sh
+
+to view modes  :
+ls -al
